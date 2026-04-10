ea152650279fde25ffd82ce5f1918b32d7ec0585
visgotti <joevisca2@gmail.com>
2026-04-10 09:42:31 +0200

    export resource classes as part of the public SDK surface

--- src/index.ts
+++ src/index.ts
@@ -18,5 +18,15 @@ export {
   TimeoutError,
 } from './errors';
 
+// Resource classes (the SDK surface that works through the proxy)
+export { ChatResource, ChatCompletionsResource } from './resources/chat';
+export { EmbeddingsResource } from './resources/embeddings';
+export { ModelsResource } from './resources/models';
+export { KeysResource } from './resources/keys';
+export { UsersResource } from './resources/users';
+export { TeamsResource } from './resources/teams';
+export { BudgetsResource } from './resources/budgets';
+export { HealthResource } from './resources/health';
+
 // Re-export all types
 export type * from './types/index';
